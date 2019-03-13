601dcc8c94f09361fe2989aa8093bb4f72953cd7
Chaim Lev Ari <chiptus@gmail.com>
2019-03-13 08:46:30 +0200

    reformat

--- tzscan-parse-rewards.js
+++ tzscan-parse-rewards.js
@@ -7,19 +7,19 @@ main().catch(e => console.error(e) || process.exit(1));
 async function main() {
   const [, , cycle] = process.argv;
   if (!cycle) {
-    console.info("USAGE: parse-tzscan-rewards CYCLE")
+    console.info('USAGE: parse-tzscan-rewards CYCLE');
     process.exit(1);
     return;
   }
-  const pageSize = 50
+  const pageSize = 50;
   const fee = 5 / 100;
   const pageUrlBuilder = (cycle, page, size = pageSize) => `https://api${Math.floor(Math.random() * 6 + 1)}.tzscan.io/v3/rewards_split/${bakeryAddress}?cycle=${cycle}&p=${page}&number=${size}`
 
   const { rewards, stakingBalance, numberOfPages } = await handlePage0();
   // console.log({ rewards, stakingBalance, numberOfPages });
-  pages = Array.from({ length: numberOfPages }, (_, k) => pageUrlBuilder(cycle, k))
+  pages = Array.from({ length: numberOfPages }, (_, k) => pageUrlBuilder(cycle, k));
   const rewardsSplit = await Promise.all(pages.map(handleUrl));
-  console.log(rewardsSplit.reduce((arr, cur) => arr.concat(cur), []).join('\n'))
+  console.log(rewardsSplit.reduce((arr, cur) => arr.concat(cur), []).join('\n'));
 
   async function handlePage0() {
     const response = await fetchUrl(pageUrlBuilder(cycle, 0, 0), 0);
@@ -54,7 +54,12 @@ async function main() {
   }
 }
 
-async function retryFetch(url, tries = 10, exitOnFail = true, predicate = response => response.headers.get('content-type') === 'application/json') {
+async function retryFetch(
+  url,
+  tries = 10,
+  exitOnFail = true,
+  predicate = response => response.headers.get('content-type') === 'application/json'
+) {
   for (let i = 0; i < tries; i++) {
     let response = await fetch(url);
     if (predicate(response)) {
